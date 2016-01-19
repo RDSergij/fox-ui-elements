1bbd2b247c8467d2ff4a95d39ca37d8ea001ed50
Osadchyi Serhii <sergijlys@gmail.com>
2016-01-19 11:33:49 +0200

    Update ui-select.php

--- ui-select.php
+++ ui-select.php
@@ -1,41 +1,81 @@
 <?php
 /**
- * Switcher class
+ * Description: Fox ui-elements
+ * Version: 0.1.0
+ * Author: Osadchyi Serhii
+ * Author URI: https://github.com/RDSergij
+ *
+ * @package ui_input_fox
+ *
+ * @since 0.1.0
  */
 
-if ( ! class_exists( 'ui_select_fox' ) ) {
-	class ui_select_fox {
+if ( ! class_exists( 'UI_Select_Fox' ) ) {
+
+	/**
+	 * UI-select.
+	 */
+	class UI_Select_Fox {
+
+		/**
+		 * Default settings
+		 *
+		 * @var type array
+		 */
 		private $default_settings = array(
 			'id'        => 'select-fox',
 			'class'     => '',
 			'name'      => 'select-fox',
-			'options'   => array( 'true' => 'On', 'false'=> 'Off' ),
+			'options'   => array( 'true' => 'On', 'false' => 'Off' ),
 			'default'   => 'true',
 		);
+
+		/**
+		 * Required settings
+		 *
+		 * @var type array
+		 */
 		private $required_settings = array(
-			'class'     => 'select-fox'
+			'class'     => 'select-fox',
 		);
+
+		/**
+		 * Settings
+		 *
+		 * @var type array
+		 */
 		public $settings;
 
+		/**
+		 * Init base settings
+		 */
 		public function __construct( $attr = null ) {
 			if ( empty( $attr ) || ! is_array( $attr ) ) {
 				$attr = $this->default_settings;
 			} else {
-				foreach( $this->default_settings as $key => $value ) {
+				foreach ( $this->default_settings as $key => $value ) {
 					if ( empty( $attr[ $key ] ) ) {
 						$attr[ $key ] = $this->default_settings[ $key ];
 					}
 				}
 			}
-			
+
 			$this->settings = $attr;
 		}
 
+		/**
+		 * Add styles
+		 */
 		private function assets() {
-			$url = plugins_url( 'fox-ui-elements/assets/css/select.min.css', dirname(__FILE__) );
+			$url = plugins_url( 'fox-ui-elements/assets/css/select.min.css', dirname( __FILE__ ) );
 			wp_enqueue_style( 'select-fox', $url, array(), '0.1.0', 'all' );
 		}
 
+		/**
+		 * Render html
+		 *
+		 * @return string
+		 */
 		public function output() {
 			$this->assets();
 			foreach ( $this->required_settings as $key => $value ) {
@@ -52,7 +92,7 @@ if ( ! class_exists( 'ui_select_fox' ) ) {
 				unset( $this->settings['default'] );
 			}
 			foreach ( $this->settings as $key => $value ) {
-				$attributes.= ' ' . $key . '="' . $value . '"';
+				$attributes .= ' ' . $key . '="' . $value . '"';
 			}
 
 			ob_start();
